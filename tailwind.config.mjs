2a8e06a751ad04c36d069f2bfaba84f9b529ab65
tsquix <tsquix@gmail.com>
2024-08-11 18:13:12 +0200

    dropdown-menus

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,28 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      colors: {
+        "almost-white": "hsl(0, 0%, 98%)",
+        "medium-gray": "hsl(0, 0%, 41%)",
+        "almost-black": "hsl(0, 0%, 8%)",
+      },
+      fontFamily: {
+        sans: ["Epilogue", "sans-serif"],
+      },
+      fontWeight: {
+        normal: 500,
+        bold: 700,
+      },
+      fontSize: {
+        headingL: ["80px", "80px"],
+        BodyS: ["14px", "16px"],
+      },
+      boxShadow: {
+        custom: "4px 1px 8px rgba(0, 0, 0, 0.1)",
+      },
+    },
+  },
+  plugins: [],
+};
