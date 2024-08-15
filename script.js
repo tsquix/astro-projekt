f093d1f53929d9dfe2a1fc5d5c85693922125b99
tsquix <tsquix@gmail.com>
2024-08-15 15:23:39 +0200

    Updates

---
+++ script.js
@@ -0,0 +1,51 @@
+"use strict";
+document.addEventListener("DOMContentLoaded", () => {
+  gsap.from("body", { duration: 1, opacity: 0, y: 20 });
+});
+document.addEventListener("DOMContentLoaded", function () {
+  const hamburgerMenu = document.getElementById("hamburgerMenu");
+  const mobileMenu = document.getElementById("mobileMenu");
+  const mask = document.getElementById("mask");
+  const closeMenu = document.getElementById("closeMenu");
+
+  hamburgerMenu.addEventListener("click", function () {
+    if (mobileMenu.classList.contains("translate-x-full")) {
+      mobileMenu.classList.remove("translate-x-full");
+      mobileMenu.classList.add("translate-x-0");
+      mask.classList.add("opacity-50");
+      mask.classList.remove("pointer-events-none");
+    } else {
+      mobileMenu.classList.add("translate-x-full");
+      mobileMenu.classList.remove("translate-x-0");
+      mask.classList.remove("opacity-50");
+      mask.classList.add("pointer-events-none");
+    }
+  });
+
+  mask.addEventListener("click", function () {
+    mobileMenu.classList.add("translate-x-full");
+    mobileMenu.classList.remove("translate-x-0");
+    mask.classList.remove("opacity-50");
+    mask.classList.add("pointer-events-none");
+  });
+
+  closeMenu.addEventListener("click", function () {
+    mobileMenu.classList.add("translate-x-full");
+    mobileMenu.classList.remove("translate-x-0");
+    mask.classList.remove("opacity-50");
+    mask.classList.add("pointer-events-none");
+  });
+
+  const featuresBtnMobile = document.getElementById("featuresBtnMobile");
+  const companyBtnMobile = document.getElementById("companyBtnMobile");
+  const dropdownMenuMobile = document.getElementById("dropdown-menu-mobile");
+  const dropdownMenu2Mobile = document.getElementById("dropdown-menu2-mobile");
+
+  featuresBtnMobile.addEventListener("click", function () {
+    dropdownMenuMobile.classList.toggle("hidden");
+  });
+
+  companyBtnMobile.addEventListener("click", function () {
+    dropdownMenu2Mobile.classList.toggle("hidden");
+  });
+});
